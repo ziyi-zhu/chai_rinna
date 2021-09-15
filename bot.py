8ea642eefd2b48f2a0e4dee9e2a3b7d41eadb1ec
Ziyi Zhu <zhu.ziyi@outlook.com>
2021-09-15 09:34:04 +0100

    Add gifs and credentials

--- bot.py
+++ bot.py
@@ -2,34 +2,33 @@ from chai_py import ChaiBot, Update
 import npu
 import json
 import requests
-from emoticons import emoticons
 import random
 
-BOT_NAME = "Rinna"
-USR_NAME = "Person"
+from resources import emoticons, gifs
+from credentials import API_TOKEN_NEURO, API_TOKEN
 
-API_TOKEN = 'api_pqqqNkmGKXbDmIVgwoXLBAPmyiAouNTlKP'
-API_URL = "https://api-inference.huggingface.co/models/bhadresh-savani/distilbert-base-uncased-emotion"
 
 class Bot(ChaiBot):
     def setup(self):
         self.logger.info("Starting setup...")
         self.chat_log = []
-        npu.api('LsqV0Z2PP-_A_tceb_Mtkx16i1r1knOrxW3_Q0b-HD8', deployed=True)
+        npu.api(API_TOKEN_NEURO, deployed=True)
         self.model_id = '60ca2a1e54f6ecb69867c72c'
         self.kwargs = {
             'response_length': 50,
             'remove_input': True,
             'do_sample': True,
-            'top_p': 0.95,
             'temperature': 0.9
         }
+        self.bot_name = "Rinna"
+        self.user_name = "Person"
+        self.api_url = "https://api-inference.huggingface.co/models/bhadresh-savani/distilbert-base-uncased-emotion"
         self.headers = {"Authorization": f"Bearer {API_TOKEN}"}
-        self.context = f'{BOT_NAME} is 16 years old. {BOT_NAME} is a high school girl living in Tokyo, Japan. She loves to watch anime and read manga. She is cute and friendly.\n'
+        self.context = f'{self.bot_name} is 16 years old. {self.bot_name} is a high school girl living in Tokyo, Japan. She loves to watch anime and read manga. She is cute and friendly.\n'
 
     def query(self, payload):
         data = json.dumps(payload)
-        response = requests.request("POST", API_URL, headers=self.headers, data=data)
+        response = requests.request("POST", self.api_url, headers=self.headers, data=data)
         return json.loads(response.content.decode("utf-8"))
 
     def get_message(self, text):
@@ -39,7 +38,7 @@ class Bot(ChaiBot):
                 quote_count += 1
                 if quote_count % 2 == 1 and i+1 < len(text) and not text[i+1].isalpha() and not text[i+1].isnumeric():
                     break
-        j = text.find(f'{USR_NAME} said:')
+        j = text.find(f'{self.user_name} said:')
         if j == -1:
             j = len(text)
         return text[:min(i, j-1)]
@@ -47,29 +46,34 @@ class Bot(ChaiBot):
     async def on_message(self, update: Update) -> str:
         user_reply = update.latest_message.text
         if user_reply == self.FIRST_MESSAGE_STRING:
-            self.chat_log.append((BOT_NAME, f'Hellooo! :) <3 I\'m {BOT_NAME}.'))
-            self.chat_log.append((USR_NAME, f'Hi! Nice to meet you.'))
-            self.chat_log.append((BOT_NAME, f'Lovely to meet you too! <3 Tell me a little story about today?'))
-            return f'Hellooo! :) <3 I\'m {BOT_NAME}, tell me a little story about today?'
+            self.chat_log.append((self.bot_name, f'Hellooo! :) <3 I\'m {self.bot_name}.'))
+            self.chat_log.append((self.user_name, f'Hi! Nice to meet you.'))
+            self.chat_log.append((self.bot_name, f'Lovely to meet you too! <3 Tell me a little story about today?'))
+            return f'Hellooo! :) <3 I\'m {self.bot_name}, tell me a little story about today?' + f'\n\n![](https://media.giphy.com/media/PO1BMFxn6E1ck/giphy.gif)'
 
-        self.chat_log.append((USR_NAME, user_reply))
+        self.chat_log.append((self.user_name, user_reply))
         history = '\n'.join([f'{sender} said: "{message}"' for (sender, message) in self.chat_log])
         # print(history)
-        data = [self.context + history + f'\n{BOT_NAME} said: "']
+        data = [self.context + history + f'\n{self.bot_name} said: "']
 
         output = npu.predict(self.model_id, data, self.kwargs)
         text = output[0]['generated_text']
 
         response = self.get_message(text)
-        self.chat_log.append((BOT_NAME, response))
+        self.chat_log.append((self.bot_name, response))
 
-        if random.uniform(0, 1) > 0.5:
-            try:
-                data = self.query({"inputs": response})
-                results = [(pred['score'], pred['label']) for pred in data[0]]
-                emoticon = random.choice(emoticons[max(results)[1]])
+        try:
+            data = self.query({"inputs": response})
+            results = [(pred['score'], pred['label']) for pred in data[0]]
+            score, emotion = max(results)
+            print(score, emotion)
+            if score > 0.95:
+                gif = random.choice(gifs[emotion])
+                response += f'\n\n![]({gif})'
+            elif score > 0.5:
+                emoticon = random.choice(emoticons[emotion])
                 response += f' {emoticon}'
-            except: 
-                pass
+        except: 
+            pass
 
         return response
---
+++ resources.py
@@ -0,0 +1,76 @@
+emoticons = {
+    'sadness': [
+        '(╯_╰)',
+        '⊙︿⊙',
+        '(ノ_<、)',
+        '╥﹏╥',
+        '‧º·(˚ ˃̣̣̥⌓˂̣̣̥ )‧º·',
+        '(⋟﹏⋞)'
+    ],
+    'joy': [
+        '(ᗒᗨᗕ)',
+        '°˖✧◝(⁰▿⁰)◜✧˖°',
+        '╰(▔∀▔)╯',
+        '(≧▽≦)',
+        '（＾ｖ＾）',
+        '⊙▽⊙'
+    ],
+    'love': [
+        '♡＾▽＾♡',
+        '(｡・//ε//・｡)',
+        '(´｡• ᵕ •｡`) ♡',
+        '♡＾▽＾♡',
+        '(´• ω •`) ♡',
+        '(灬♥ω♥灬)'
+    ],
+    'anger': [
+        '(¬､¬)',
+        '(╯°□°）╯',
+        '༽◺_◿༼',
+        '-`д´-',
+        '┬─┬ノ(ಠ_ಠノ)',
+        '( ≧Д≦)'
+    ],
+    'fear': [
+        'ゞ◎Д◎ヾ',
+        '（ΩДΩ）',
+        'ჴ˘ര‸രჴ',
+        '(⊃д⊂)',
+        '╭(°ﾛ° ”)╯',
+        'џ(ºДºџ)'
+    ],
+    'surprise': [
+        '∑(; °Д°)',
+        '(⚆ᗝ⚆)',
+        '〣 ( ºΔº ) 〣',
+        '(✿〇∀〇)',
+        '╭( ๐_๐)╮',
+        '(○（●●）○)'
+    ] 
+}
+
+gifs = {
+    'sadness': [
+        'https://media.giphy.com/media/3o6nV9ThwcDvNmRNe0/giphy.gif'
+    ],
+    'joy': [
+        'https://media.giphy.com/media/3ohjUY0WA2e0E5wpdC/giphy.gif',
+        'https://media1.tenor.com/images/c191276beafa554a47232b4abeb70b3b/tenor.gif',
+        'https://media.giphy.com/media/xT1Ra3VzNv3qlDouyY/giphy.gif',
+        'https://media.giphy.com/media/26n8D5MJ2jfBgtyA8/giphy.gif',
+        'https://media.giphy.com/media/5MhQ86ZwtUjGU/giphy.gif'
+    ],
+    'love': [
+        'https://media.giphy.com/media/8vg9C3QP19bc4/giphy.gif',
+        'https://media1.tenor.com/images/b058328e646c89f5aba3f964e7e0b51d/tenor.gif'
+    ],
+    'anger': [
+        'https://media.giphy.com/media/HvBCQahj6aN5MQuwbN/giphy.gif'
+    ],
+    'fear': [
+        'https://media.giphy.com/media/3ohjV8hsNSWStOaT1S/giphy.gif'
+    ],
+    'surprise': [
+        'https://media.giphy.com/media/l4EoPRk7d1mmrv8AM/giphy.gif'
+    ] 
+}
